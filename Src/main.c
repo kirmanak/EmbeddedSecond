19a06a42c76188f1d0e7b8e08b22ac609910203f
kirmanak <k.kamakin@protonmail.ch>
2019-10-17 21:46:00 +0300

    Initial commit
    Signed-off-by: kirmanak <k.kamakin@protonmail.ch>

---
+++ Src/main.c
@@ -0,0 +1,333 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include<string.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define GREEN GPIO_PIN_13
+#define YELLOW GPIO_PIN_14
+#define RED GPIO_PIN_15
+#define BUTTON GPIO_PIN_15
+
+#define GREEN_STATE 0
+#define GREEN_BLINK_STATE 1
+#define YELLOW_STATE 2
+#define RED_STATE 3
+
+#define EOL "\n\r"
+#define DELAY 1000
+#define DEFAULT_TIMEOUT 3000
+
+#define MSG_REPLACE "Please, enter new combination"
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart4;
+UART_HandleTypeDef huart6;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+
+static void MX_GPIO_Init(void);
+
+static void MX_UART4_Init(void);
+
+static void MX_USART6_UART_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+struct state {
+    long red_timeout;
+    uint32_t last_switch_time;
+    int is_interrupt_on;
+    int last_color;
+};
+
+void question() {
+    HAL_UART_Transmit(&huart6, MSG_REPLACE, strlen(MSG_REPLACE), DELAY);
+    HAL_UART_Transmit(&huart6, EOL, strlen(EOL), DELAY);
+}
+
+int should_set_color(struct state *current_state) {
+    long timeout = current_state->last_color == RED ? current_state->red_timeout : DEFAULT_TIMEOUT;
+    uint32_t current_tick = HAL_GetTick();
+    uint32_t current_time = current_tick / 1000;
+    uint32_t color_time_end = current_state->last_switch_time + timeout;
+    return current_time >= color_time_end;
+}
+
+void show_color(int color) {
+    switch (color) {
+        case GREEN:
+            HAL_GPIO_WritePin(GPIOD, YELLOW, GPIO_PIN_RESET);
+            HAL_GPIO_WritePin(GPIOD, RED, GPIO_PIN_RESET);
+            break;
+
+        case YELLOW:
+            HAL_GPIO_WritePin(GPIOD, GREEN, GPIO_PIN_RESET);
+            HAL_GPIO_WritePin(GPIOD, RED, GPIO_PIN_RESET);
+            break;
+        case RED:
+            HAL_GPIO_WritePin(GPIOD, YELLOW, GPIO_PIN_RESET);
+            HAL_GPIO_WritePin(GPIOD, GREEN, GPIO_PIN_RESET);
+            break;
+    }
+    HAL_GPIO_WritePin(GPIOD, color, GPIO_PIN_SET);
+}
+
+void set_next_color(const struct state *current_state) {
+    switch (current_state->last_color) {
+        case GREEN_STATE:
+            break;
+
+        case GREEN_BLINK_STATE:
+            break;
+
+        case YELLOW:
+            break;
+
+        case RED:
+            break;
+    }
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void) {
+    /* USER CODE BEGIN 1 */
+
+    /* USER CODE END 1 */
+
+
+    /* MCU Configuration--------------------------------------------------------*/
+
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init();
+
+    /* USER CODE BEGIN Init */
+
+    /* USER CODE END Init */
+
+    /* Configure the system clock */
+    SystemClock_Config();
+
+    /* USER CODE BEGIN SysInit */
+
+    /* USER CODE END SysInit */
+
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_UART4_Init();
+    MX_USART6_UART_Init();
+    /* USER CODE BEGIN 2 */
+
+    /* USER CODE END 2 */
+
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
+    struct state current_state = {
+            .red_timeout = DEFAULT_TIMEOUT,
+            .last_switch_time = 0,
+            .is_interrupt_on = 0,
+            .last_color = RED
+    };
+    while (1) {
+        if (should_set_color(&current_state)) {
+            set_next_color(&current_state);
+        }
+        /* USER CODE END WHILE */
+
+        /* USER CODE BEGIN 3 */
+    }
+    /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void) {
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+    /** Configure the main internal regulator output voltage
+    */
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+    /** Initializes the CPU, AHB and APB busses clocks
+    */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
+    /** Initializes the CPU, AHB and APB busses clocks
+    */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+                                  | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
+        Error_Handler();
+    }
+}
+
+/**
+  * @brief UART4 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_UART4_Init(void) {
+
+    /* USER CODE BEGIN UART4_Init 0 */
+
+    /* USER CODE END UART4_Init 0 */
+
+    /* USER CODE BEGIN UART4_Init 1 */
+
+    /* USER CODE END UART4_Init 1 */
+    huart4.Instance = UART4;
+    huart4.Init.BaudRate = 115200;
+    huart4.Init.WordLength = UART_WORDLENGTH_8B;
+    huart4.Init.StopBits = UART_STOPBITS_1;
+    huart4.Init.Parity = UART_PARITY_NONE;
+    huart4.Init.Mode = UART_MODE_TX_RX;
+    huart4.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart4.Init.OverSampling = UART_OVERSAMPLING_16;
+    if (HAL_UART_Init(&huart4) != HAL_OK) {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN UART4_Init 2 */
+
+    /* USER CODE END UART4_Init 2 */
+
+}
+
+/**
+  * @brief USART6 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART6_UART_Init(void) {
+
+    /* USER CODE BEGIN USART6_Init 0 */
+
+    /* USER CODE END USART6_Init 0 */
+
+    /* USER CODE BEGIN USART6_Init 1 */
+
+    /* USER CODE END USART6_Init 1 */
+    huart6.Instance = USART6;
+    huart6.Init.BaudRate = 115200;
+    huart6.Init.WordLength = UART_WORDLENGTH_8B;
+    huart6.Init.StopBits = UART_STOPBITS_1;
+    huart6.Init.Parity = UART_PARITY_NONE;
+    huart6.Init.Mode = UART_MODE_TX_RX;
+    huart6.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart6.Init.OverSampling = UART_OVERSAMPLING_16;
+    if (HAL_UART_Init(&huart6) != HAL_OK) {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN USART6_Init 2 */
+
+    /* USER CODE END USART6_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void) {
+
+    /* GPIO Ports Clock Enable */
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void) {
+    /* USER CODE BEGIN Error_Handler_Debug */
+    /* User can add his own implementation to report the HAL error return state */
+
+    /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
